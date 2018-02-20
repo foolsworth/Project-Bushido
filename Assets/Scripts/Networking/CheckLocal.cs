b2055252f92f077741f27cfa1e11681938a6f8fd
hiredprotection <joe_nahas@hotmail.com>
2018-02-20 11:39:23 -0500

    Photon

---
+++ Assets/Scripts/Networking/CheckLocal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLocal : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (photonView.isMine == false && PhotonNetwork.connected == true)
+        {
+            gameObject.transform.Find("Head").GetComponent<lerpGhostHands>().enabled = false;
+            gameObject.transform.Find("Left").GetComponent<lerpGhostHands>().enabled = false;
+            gameObject.transform.Find("Right").GetComponent<lerpGhostHands>().enabled = false;
+            gameObject.transform.Find("Right").GetComponent<grabsword>().enabled = false;
+            gameObject.transform.Find("Body").GetComponent<lerpBody>().enabled = false;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : Photon.PunBehaviour
+{
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+
+
+    private void Start()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.Log("We are Instantiating LocalPlayer from " + SceneManager.GetActiveScene().name);
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+    }
+
+    void LoadScene()
+    {
+        if (!PhotonNetwork.isMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+       
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerConnected() " + other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+            LoadScene();
+        }
+    }
+
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+    {
+        Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("OnPhotonPlayerDisonnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+
+            LoadScene();
+        }
+    }
+
+}
