e593d6064bda1bbb9e670fc60927b55c0c9bd6f9
hiredprotection <joe_nahas@hotmail.com>
2018-02-27 12:28:02 -0500

    Testing again

---
+++ Assets/Scripts/Networking/OurLauncher.cs
@@ -0,0 +1,151 @@
+﻿
+using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+    public class OurLauncher : Photon.PunBehaviour
+    {
+
+        [Tooltip("The maximum number of players per room")]
+        public byte maxPlayersPerRoom = 2;
+
+      
+        bool isConnecting;
+
+        string _gameVersion = "1";
+
+        void Awake()
+        {
+            
+
+            // #Critical
+            // we don't join the lobby. There is no need to join a lobby to get the list of rooms.
+            PhotonNetwork.autoJoinLobby = false;
+
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.automaticallySyncScene = true;
+
+
+        }
+
+    
+
+    
+        public void Connect()
+        {
+        
+
+            // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
+            isConnecting = true;
+
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.connected)
+            {
+              
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+
+          
+
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+            }
+        }
+    
+
+      
+
+
+
+        /// <summary>
+        /// Called after the connection to the master is established and authenticated but only when PhotonNetwork.autoJoinLobby is false.
+        /// </summary>
+        public override void OnConnectedToMaster()
+        {
+
+            Debug.Log("Region:" + PhotonNetwork.networkingPeer.CloudRegion);
+
+            // we don't want to do anything if we are not attempting to join a room. 
+            // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
+            // we don't want to do anything.
+            if (isConnecting)
+            {
+                Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room.\n Calling: PhotonNetwork.JoinRandomRoom(); Operation will fail if no room found");
+
+                // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnPhotonRandomJoinFailed()
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+
+        /// <summary>
+        /// Called when a JoinRandom() call failed. The parameter provides ErrorCode and message.
+        /// </summary>
+        /// <remarks>
+        /// Most likely all rooms are full or no rooms are available. <br/>
+        /// </remarks>
+        /// <param name="codeAndMsg">codeAndMsg[0] is short ErrorCode. codeAndMsg[1] is string debug msg.</param>
+        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+        {
+           
+            Debug.Log("DemoAnimator/Launcher:OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
+
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = this.maxPlayersPerRoom }, null);
+        }
+
+
+        /// <summary>
+        /// Called after disconnecting from the Photon server.
+        /// </summary>
+        /// <remarks>
+        /// In some cases, other callbacks are called before OnDisconnectedFromPhoton is called.
+        /// Examples: OnConnectionFail() and OnFailedToConnectToPhoton().
+        /// </remarks>
+        public override void OnDisconnectedFromPhoton()
+        {
+            
+            Debug.LogError("DemoAnimator/Launcher:Disconnected");
+
+     
+
+            isConnecting = false;
+           
+
+        }
+
+        /// <summary>
+        /// Called when entering a room (by creating or joining it). Called on all clients (including the Master Client).
+        /// </summary>
+        /// <remarks>
+        /// This method is commonly used to instantiate player characters.
+        /// If a match has to be started "actively", you can call an [PunRPC](@ref PhotonView.RPC) triggered by a user's button-press or a timer.
+        ///
+        /// When this is called, you can usually already access the existing players in the room via PhotonNetwork.playerList.
+        /// Also, all custom properties should be already available as Room.customProperties. Check Room..PlayerCount to find out if
+        /// enough players are in the room to start playing.
+        /// </remarks>
+        public override void OnJoinedRoom()
+        {
+            
+            Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.\nFrom here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
+
+            // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.automaticallySyncScene to sync our instance scene.
+            if (PhotonNetwork.room.PlayerCount == 1)
+            {
+                Debug.Log("We load the game ");
+
+                // #Critical
+                // Load the Room Level. 
+                PhotonNetwork.LoadLevel("Game");
+
+            }
+        }
+
+
+    }
