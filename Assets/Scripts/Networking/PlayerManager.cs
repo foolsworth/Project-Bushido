6b9bbb4e577d90fff0284ae97020435325b78665
hiredprotection <joe_nahas@hotmail.com>
2018-02-27 11:35:58 -0500

    testing mroe stuff

--- Assets/Scripts/Networking/PlayerManager.cs
+++ Assets/Scripts/Networking/PlayerManager.cs
@@ -7,9 +7,6 @@ using UnityEngine.EventSystems;
 public class PlayerManager : Photon.PunBehaviour
 {
 
-    [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
-    public static GameObject LocalPlayerInstance;
-
 
 
     public void Awake()
@@ -19,8 +16,8 @@ public class PlayerManager : Photon.PunBehaviour
         // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
         if (photonView.isMine)
         {
-            LocalPlayerInstance = gameObject;
-            LocalPlayerInstance.tag = "local";
+            GameManager.LocalPlayerInstance = gameObject;
+            GameManager.LocalPlayerInstance.tag = "local";
         }
 
         // #Critical
