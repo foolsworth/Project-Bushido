96fec08c2ae317bb54673dfdcea66845a146624a
asailijhijr <asailijhijr@gmail.com>
2018-03-06 12:43:21 -0500

    Added SceneTransitionScene.scene
    Kevin's scene transition scene, this is ready for VR testing.

---
+++ Assets/Scripts/SceneTransitionScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionScript : MonoBehaviour {
+    //publicly accessible variables
+    public float terminus = -75.0f;
+    public float waitTime = 5.0f;
+    public float duration = 5.0f;
+    public Transform yourFace;
+
+    //Make this the capsule
+    public GameObject tunnel;
+
+    float accumulatedTime;
+    bool destinationReached;
+    //Lerping variables
+    Vector3 source;
+    Vector3 destination;
+    float lerpTime;
+    float destinationTime;
+
+	// Use this for initialization
+	void Start () {
+        //initialise variables
+        accumulatedTime = 0;
+        lerpTime = 0;
+        destinationReached = false;
+        source = tunnel.transform.position;
+        destination = new Vector3(tunnel.transform.position.x, tunnel.transform.position.y, tunnel.transform.position.z + terminus);
+        destinationTime = float.PositiveInfinity;
+        if(!yourFace) {
+            yourFace = Camera.main.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Nothing in the Update() method.
+        Debug.Log(Camera.main.transform.position);
+	}
+
+    private void FixedUpdate() {
+        //Make accumulatedTime accurate.
+        accumulatedTime += Time.deltaTime;
+        //update Lerping variables
+        //lerpTime = ((destinationReached) ? (accumulatedTime - destinationTime) : ((accumulatedTime >= waitTime) ? (accumulatedTime - waitTime) : (0.0f)));
+        lerpTime = ((destinationReached) ? ((accumulatedTime - waitTime) / duration) : ((accumulatedTime >= waitTime) ? ((accumulatedTime - waitTime - duration) / 1.0f) : (0.0f)));
+        //update the Lerps
+        tunnel.transform.position = new Vector3(Mathf.Lerp(source.x, destination.x, lerpTime * Time.deltaTime), Mathf.Lerp(source.y, destination.y, lerpTime * Time.deltaTime), Mathf.Lerp(source.z, destination.z, lerpTime * Time.deltaTime));
+        //set destination
+        if(Vector3.Distance(tunnel.transform.position, destination) <= 0.001f && !destinationReached) {
+            destinationReached = true;
+            source = tunnel.transform.position;
+            destination = yourFace.position;
+        }
+
+        /*//check if waitTime is accurate
+        if (accumulatedTime >= waitTime) {
+            //check if we're there already
+            if (tunnel.transform.position.z != terminus) {
+                //linearly interpolate our current position from our current position to the goal position according to accumulatedTime after the wait.
+                tunnel.transform.position = new Vector3(tunnel.transform.position.x, tunnel.transform.position.y, Mathf.Lerp(tunnel.transform.position.z, terminus, (accumulatedTime - waitTime) * Time.deltaTime));
+            } else {
+                //If we are, say so
+                destinationReached = true;
+            }
+        }
+
+        if(accumulatedTime >= waitTime && destinationReached) {
+
+            }*/
+    }
+
+    private Vector3 LerpPosition(Vector3 source, Vector3 destination, float timeValue) {
+        return new Vector3(Mathf.Lerp(source.x, destination.x, timeValue), Mathf.Lerp(source.y, destination.y, timeValue), Mathf.Lerp(source.z, destination.z, timeValue));
+    }
+}
---
+++ Assets/Scripts/SceneTransitionScript_v2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionScript_v2 : MonoBehaviour {
+    //The time since this object was created
+    private float m_accumulatedTime;
+
+    //necessary gameObjects
+    public GameObject m_tunnel;
+    public GameObject m_yourFace;
+
+    //parameters
+    public float m_waitTime = 5.0f; //This will be overridden in the editor if a different value is given.
+    public float m_duration = 5.0f; //The number of seconds that the movement should take.
+
+    //Calculation area
+    private Vector3 m_source;
+    private Vector3 m_destination;
+    private Vector3 m_destinationOffset;
+
+	// Use this for initialization
+	void Start () {
+        m_accumulatedTime = 0.0f;
+        m_source = m_tunnel.transform.position; //I will need to copy the values (as there is no Vector3 copy constructor) if this is changing from frame to frame.
+        m_destinationOffset = new Vector3(0.0f, 0.0f, (-0.75f * m_tunnel.transform.localScale.y));
+        m_destination = new Vector3(m_yourFace.transform.position.x, m_yourFace.transform.position.y, m_yourFace.transform.position.z);
+        m_destination += m_destinationOffset;
+        Debug.Log("Dstination is: " + m_destination);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Isn't it changing? \t " + m_source); //I want this not to be changing.
+        Debug.Log("Is it changing? \t " + m_destination);  //I want this to be changing.
+    }
+
+    private void FixedUpdate() {
+        m_accumulatedTime += Time.fixedDeltaTime;
+        //m_destination = m_yourFace.transform.position; //If I'm wrong about this being a pointer, I don't need this line.
+
+        //if(m_accumulatedTime <= m_waitTime + m_duration) { //Disable movement if the action is finished.
+        m_tunnel.transform.position = LerpPosition(m_source, m_destination, (m_accumulatedTime - m_waitTime) / m_duration);
+        //Debug.Log("Current Position is: " + m_tunnel.transform.position);
+        // } else {
+        //    m_tunnel.transform.position = m_yourFace.transform.position;
+        // }
+    }
+
+    private Vector3 LerpPosition(Vector3 source, Vector3 destination, float timeValue) {
+        return new Vector3(Mathf.Lerp(source.x, destination.x, timeValue), Mathf.Lerp(source.y, destination.y, timeValue), Mathf.Lerp(source.z, destination.z, timeValue));
+    }
+}
