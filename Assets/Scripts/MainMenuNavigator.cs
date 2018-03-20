9f886cc5d097ef93ce4dbe4726fe6337bab095d9
asailijhijr <asailijhijr@gmail.com>
2018-03-20 17:44:03 -0400

    Created MainMenu Scene.
    Created all of the things needed for the main menu scene, all placeholder assets presently.

---
+++ Assets/Scripts/MainMenuNavigator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuNavigator : MonoBehaviour {
+    //I'm thinking this script should be on the candle or on the flame.
+
+    public SceneTransitionScript_v3 m_transitionController;
+
+    private void Start() {
+        
+        //set the variables to infinity
+        //m_transitionController.m_waitTime = float.PositiveInfinity;
+        //m_transitionController.m_animationDuration = float.PositiveInfinity;
+        //pause the transition
+        m_transitionController.PauseTransition();
+    }
+
+    private void OnTriggerEnter(Collider p_collider) {
+        if (p_collider.transform.CompareTag("Menu_Proceed")) {
+            //do the proceed thing
+            //load the scene or whatever.
+            Debug.Log("Menu option \"Proceed\" was selected.");
+            //unpause the transition
+            m_transitionController.PauseTransition(true);
+
+        }
+        if(p_collider.transform.CompareTag("Menu_Option0")) {
+            //do the options thing
+            //load the scene or whatever
+            Debug.Log("Menu option \"Options\" was selected.");
+            m_transitionController.PauseTransition(true);
+        }
+    }
+
+    private void setTransitionLength(float givenDuration) {
+        m_transitionController.m_animationDuration = givenDuration;
+    }
+}
--- Assets/Scripts/SceneTransitionScript_v3.cs
+++ Assets/Scripts/SceneTransitionScript_v3.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SceneTransitionScript_v3 : MonoBehaviour {
 
     float m_accumulatedTime;
+    bool m_doesAccumulate;
 
     GameObject m_tunnelParent;
     public GameObject m_player;
+    public GameObject[] m_otherRotators;
 
     //parameters  //These will be overridden in the editor if a different value is given.
     public float m_waitTime = 5.0f;
@@ -24,19 +26,26 @@ public class SceneTransitionScript_v3 : MonoBehaviour {
 
         m_source = m_tunnelParent.transform.position; //Desired is a copy of present values.
         m_destination = m_player.transform.position;
-
-        //set the rotation of the prop/tunnel so that the sphere/"opening" is in front of the player
+        
+        //Set up the variables we need to rotate the tunnel
         Vector3 l_desiredDirection = Vector3.ProjectOnPlane(m_player.transform.forward, Vector3.up);
         float l_angle = Vector3.SignedAngle(Vector3.forward, l_desiredDirection, Vector3.up);
         //Quaternion l_rotation = Quaternion.Euler(0.0f, l_angle, 0.0f); //This is not needed in this version
-
+        //set the rotation of the prop/tunnel so that the sphere/"opening" is in front of the player
         m_tunnelParent.transform.RotateAround(m_destination, Vector3.up, l_angle);
+        foreach (GameObject rotator in m_otherRotators)
+            rotator.transform.RotateAround(m_destination, Vector3.up, l_angle);
 	}
 
     private void FixedUpdate() {
-        m_accumulatedTime += Time.fixedDeltaTime;
+        if(m_doesAccumulate)
+            m_accumulatedTime += Time.fixedDeltaTime;
         m_destination = m_player.transform.position;
 
         m_tunnelParent.transform.position = Vector3.Lerp(m_source, m_destination, (m_accumulatedTime - m_waitTime) / m_animationDuration);
+    }
+
+    public void PauseTransition(bool unpause = false) {
+        m_doesAccumulate = !unpause;
     }
 }
