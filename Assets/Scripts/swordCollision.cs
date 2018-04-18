94ed06fc4cd4fed063406dc3eae82d000bcacf6a
Kai <foolsworth@users.noreply.github.com>
2018-04-18 19:41:56 -0400

    sdf

--- Assets/Scripts/swordCollision.cs
+++ Assets/Scripts/swordCollision.cs
@@ -13,7 +13,7 @@ public class swordCollision : MonoBehaviour {
         {
             Vector3 myVel = transform.parent.GetComponent<Rigidbody>().velocity;
 
-            float force = 10;
+            float force = 100;
             swordhit.Stop();
             swordhit.Play();
             //transform.parent.GetComponent<Rigidbody>().velocity += c.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity;
@@ -27,9 +27,9 @@ public class swordCollision : MonoBehaviour {
             GetComponent<Rigidbody>().AddForceAtPosition(dir * force, c.contacts[0].point);
             rightH.GetComponent<SpringJoint>().spring = 0;
             rightH.GetComponent<SpringJoint>().damper = 0;
-            rightH.GetComponent<SpringJoint>().tolerance = 2000000000;
-            rightH.GetComponent<SpringJoint>().maxDistance = 2000000000;
-            rightH.GetComponent<SpringJoint>().minDistance = 2000000000;
+            rightH.GetComponent<SpringJoint>().tolerance = 2e20F;
+            rightH.GetComponent<SpringJoint>().maxDistance = 2e20F;
+            rightH.GetComponent<SpringJoint>().minDistance = 2e20F;
 
             //GetComponent<Rigidbody>().drag = 100;
 
