3d62e12dcfd66b10099a5d6e2eeaa80cfee10dba
Kai <foolsworth@users.noreply.github.com>
2018-04-18 18:36:00 -0400

    szdfv

--- Assets/Scripts/swordCollision.cs
+++ Assets/Scripts/swordCollision.cs
@@ -6,28 +6,34 @@ public class swordCollision : MonoBehaviour {
 
     public AudioSource swordhit;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision c)
     {
-        if (collision.collider.gameObject.layer == 11)
+        if (c.collider.gameObject.layer == 10)
         {
             Vector3 myVel = transform.parent.GetComponent<Rigidbody>().velocity;
-            
 
+            float force = 3;
             swordhit.Stop();
             swordhit.Play();
-            transform.parent.GetComponent<Rigidbody>().velocity += collision.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity;
-            collision.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity += myVel;
+            //transform.parent.GetComponent<Rigidbody>().velocity += c.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity;
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = c.contacts[0].point - transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir * force);
 
         }
-        if (collision.collider.gameObject.layer == 13)
-        {
-            Vector3 myVel = transform.parent.GetComponent<Rigidbody>().velocity;
-            collision.collider.transform.parent.GetComponent<OurPlayerManager>().takeDamage(10);
-            swordhit.Stop();
-            swordhit.Play();
-            transform.parent.GetComponent<Rigidbody>().velocity += collision.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity;
-            collision.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity += myVel;
-        }
+        //if (collision.collider.gameObject.layer == 13)
+        //{
+        //    Vector3 myVel = transform.parent.GetComponent<Rigidbody>().velocity;
+        //    collision.collider.transform.parent.GetComponent<OurPlayerManager>().takeDamage(10);
+        //    swordhit.Stop();
+        //    swordhit.Play();
+        //    transform.parent.GetComponent<Rigidbody>().velocity += collision.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity;
+        //    collision.collider.gameObject.transform.parent.GetComponent<Rigidbody>().velocity += myVel;
+        //}
     }
 
     // Use this for initialization
